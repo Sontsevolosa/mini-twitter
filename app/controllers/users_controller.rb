dcd76e27fc83721ada2ebba63aee0734f8254111
sontsevolosa <sontsevolosa@gmail.com>
2016-09-09 18:32:34 +0300

    Deleting flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Yahoo! Welcome to the Mini Twitter!'
       redirect_to @user
     else
       render 'new'
