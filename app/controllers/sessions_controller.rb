d5e64f45ed96a761f67ac50004d96ca75b87b2ee
sontsevolosa <sontsevolosa@gmail.com>
2016-09-09 19:17:38 +0300

    Adding current_user to the Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:sesion][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to current_user
     else
       render 'new'
     end
