881ea6284daac980028d30793b97fa9c80a108cf
sontsevolosa <sontsevolosa@gmail.com>
2016-09-09 16:33:52 +0300

    Replacement quotes from double to single

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Test User", email: "user@email.com", password: "blabla", password_confirmation: "blabla")
+    @user = User.new(name: 'Test User', email: "user@email.com", password: 'blabla', password_confirmation: 'blabla')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = ""
+  test 'name should be present' do
+    @user.name = ''
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = "   "
+  test 'email should be present' do
+    @user.email = '   '
     assert_not @user.valid?
   end
 end
