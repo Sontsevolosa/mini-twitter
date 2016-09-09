c1578a1dda5b0c9ff285771f7d10749796c874fc
sontsevolosa <sontsevolosa@gmail.com>
2016-09-09 16:28:07 +0300

    Adding a password and its confirmation to the test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Test User", email: "user@email.com")
+    @user = User.new(name: "Test User", email: "user@email.com", password: "blabla", password_confirmation: "blabla")
   end
 
   test "should be valid" do
