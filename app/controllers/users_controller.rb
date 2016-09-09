4a30c8d89efd07757c324b951bb2f7e28658f71b
sontsevolosa <sontsevolosa@gmail.com>
2016-09-09 18:27:19 +0300

    Adding a flach message to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flach[:success] = 'Yahoo! Welcome to the Mini Twitter!'
       redirect_to @user
     else
       render 'new'
