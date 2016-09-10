91803b56c487125b7a7f9ddf38473ced39729e2a
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 22:49:40 +0300

    Adding a feed instance variable to the home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @tweet = current_user.tweets.build if logged_in?
+    if logged_in?
+      @tweet = current_user.tweets.build
+      @feed_items = current_user.feed.build if logged_in?
   end
 end
