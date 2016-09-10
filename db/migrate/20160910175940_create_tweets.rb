4367c0a1648edc76824558b719f9a8b70d9465c4
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 21:08:01 +0300

    Adding index to the table for Tweet model

--- db/migrate/20160910175940_create_tweets.rb
+++ db/migrate/20160910175940_create_tweets.rb
@@ -2,9 +2,10 @@ class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
       t.text :content
-      t.references :user, index: true, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps null: false
     end
+    add_index :tweets, [:user_id, :created_at]
   end
 end
