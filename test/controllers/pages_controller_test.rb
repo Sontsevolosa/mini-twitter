15c34a7f9c1fe45edb12c60fa387990d13db6935
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 22:41:58 +0300

    Making shanges in view for Home page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  def setup
+    @user = User.new(name: 'Test User', email: "user@email.com", password: 'blabla', password_confirmation: 'blabla')
+  end
+
   test 'should get home' do
     get :home
     assert_response :success
