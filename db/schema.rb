e146e51eac0ddd43bbcb05214d3c7acbd767fe49
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 21:11:26 +0300

    Adding validation for tweet's user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909132100) do
+ActiveRecord::Schema.define(version: 20160910175940) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tweets", ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
