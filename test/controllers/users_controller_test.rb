ddaab73ab7d93f65542e93ac84c86f10a0159363
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 13:16:03 +0300

    Making changes in assertions for Users controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = User.new(name: 'Test User', email: "user@email.com", password: 'blabla', password_confirmation: 'blabla')
+  end
+
   test 'should get new' do
     get :new
-    assert_response :success
+    assert_redirected_to login_path
   end
 end
