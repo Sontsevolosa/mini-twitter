7158cb4a8d0e583661abf285936c2a3eea9e8d16
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 21:21:33 +0300

    Ordering the tweets with default_scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tweets
+  default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   validates :email, presence: true
   has_secure_password
