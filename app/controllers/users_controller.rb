5a4ad8db7291be242c1df8563a1a0d74cc566982
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 13:22:05 +0300

    Requiring a logged-in user for the index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_user, only: [:index]
+
+  def index
+  end
 
   def show
     @user = User.find(params[:id])
