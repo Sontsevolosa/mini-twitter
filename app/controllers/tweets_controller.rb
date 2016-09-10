b815d2490a7c3ba0d8a021f7f9edb1e14d84b9be
sontsevolosa <sontsevolosa@gmail.com>
2016-09-10 22:06:29 +0300

    Adding create action for Tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,12 @@ class TweetsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      redirect_to root_url
+    else
+      render 'pages/index'
+    end
   end
 
   def destroy
