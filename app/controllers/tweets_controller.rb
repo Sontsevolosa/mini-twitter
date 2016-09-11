c50bffb18a23f5b3f41350dd73668c040b7fcbac
sontsevolosa <sontsevolosa@gmail.com>
2016-09-11 09:22:40 +0300

    Adding destroy action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @tweet = current_user.tweets.build(tweet_params)
