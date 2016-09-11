11ce955269c7ff7f2d4f5ca145da707b3f6348ea
sontsevolosa <sontsevolosa@gmail.com>
2016-09-11 15:59:44 +0300

    Implementing user.following using passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :passive_relatioships, class_name: "Relationship", foreign_key: "followed_id"
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relatioships, source: :follower
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   validates :email, presence: true
