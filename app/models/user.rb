2659fa7ac0827473e15175e27d04c67713a76e5d
sontsevolosa <sontsevolosa@gmail.com>
2016-09-11 09:48:56 +0300

    Adding the User model following associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :following, through: :active_relationships, source: :followed
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   validates :email, presence: true
