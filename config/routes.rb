40407b5e292edb8a8b8d28e0c6f06ea1ca88ee16
sontsevolosa <sontsevolosa@gmail.com>
2016-09-11 16:15:43 +0300

    Adding following and followers actions to the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :tweets, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
